bf602ae896fc3cfcfac7e816df2177840d24ff04
Emerson Pinter <dev@pinter.com.br>
2013-01-23 17:10:24 -0200

    Enable neon vfp4 instructions
    Change-Id: I657aba08ff5abf936d097ad4527744bbc97a828a

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -32,6 +32,8 @@ TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_ARCH_VARIANT_CPU := cortex-a9
 TARGET_CPU_SMP := true
 COMMON_GLOBAL_CFLAGS += -D__ARM_USE_PLD -D__ARM_CACHE_LINE_SIZE=64
+TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
 BOARD_USES_QCOM_HARDWARE := true
 
 # Krait optimizations
