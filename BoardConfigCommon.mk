f147ee8a6aa6968059c00e9b858febdb43cf9ab2
dhacker29 <davidhackerdvm@gmail.com>
2013-05-06 20:47:27 -0500

    Add TARGET_CPU_VARIANT needed by AOSP master branch

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -29,6 +29,7 @@ TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_ARCH_VARIANT_CPU := cortex-a9
+TARGET_CPU_VARIANT := cortex-a9
 TARGET_CPU_SMP := true
 COMMON_GLOBAL_CFLAGS += -D__ARM_USE_PLD -D__ARM_CACHE_LINE_SIZE=64
 TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
