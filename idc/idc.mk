8af0462e8ac5fb827a61d37a68c2961fbcd7b56a
nadlabak <pavel@doshaska.net>
2013-05-13 19:46:33 +0200

    Add gpio-keys.idc
    - needed by http://review.cyanogenmod.org/37664
    
    Change-Id: I1d9b6a4531f2e9c9512398b90c304f534bcf224c

--- idc/idc.mk
+++ idc/idc.mk
@@ -1,4 +1,5 @@
 LOCAL_PATH := device/motorola/msm8960-common
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/idc/melfas-ts.idc:system/usr/idc/melfas-ts.idc \
-    $(LOCAL_PATH)/idc/evfwd.idc:system/usr/idc/evfwd.idc
+    $(LOCAL_PATH)/idc/evfwd.idc:system/usr/idc/evfwd.idc \
+    $(LOCAL_PATH)/idc/gpio-keys.idc:system/usr/idc/gpio-keys.idc
