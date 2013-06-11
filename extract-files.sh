b96d3d112ff1875378fd9ad329914ace9cc280b6
dhacker29 <davidhackerdvm@gmail.com>
2013-06-10 21:33:40 -0500

    Setup adreno and mako blob makefiles from extract-files
    Change-Id: I22039382ef97088dfc9378394936a53541ec2c68

--- extract-files.sh
+++ extract-files.sh
@@ -61,4 +61,16 @@ for FILE in `egrep -v '(^#|^$)' ../msm8960-common/common-proprietary-files.txt`;
     fi
 done
 
+ADRENO=../../../vendor/$VENDOR/msm8960-common/Adreno200-AU_LINUX_ANDROID_JB_VANILLA_04.02.02.060.053
+if [ ! -d $ADRENO ]; then
+    mkdir -p $ADRENO
+fi
+MAKO=../../../vendor/$VENDOR/msm8960-common/mako/lib
+if [ ! -d $MAKO ]; then
+    mkdir -p $MAKO
+fi
+echo "Don't forget to add the adreno blobs from"
+echo "https://developer.qualcomm.com/download/Adreno200-AU_LINUX_ANDROID_JB_VANILLA_04.02.02.060.053.zip"
+echo "and mako blobs from https://dl.google.com/dl/android/aosp/qcom-mako-jdq39-c89670ca.tgz"
+echo "to vendor/motorola/msm8960-common"
 ../msm8960-common/setup-makefiles.sh
