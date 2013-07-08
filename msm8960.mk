229c9994637a6f817c992c5876515c619be9797d
nadlabak <pavel@doshaska.net>
2013-07-08 10:24:45 +0200

    Remove unused media properties
    Change-Id: Id0a2df30621f9afabe8fb9ca0063f04da3a29cce

--- msm8960.mk
+++ msm8960.mk
@@ -246,17 +246,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     lpa.decode=false \
     tunnel.decode=true \
     tunnel.audiovideo.decode=true \
-    lpa.use-stagefright=true \
-    media.stagefright.enable-player=true \
-    media.stagefright.enable-http=true \
-    media.stagefright.enable-aac=true \
-    media.stagefright.enable-qcp=true \
-    media.stagefright.enable-fma2dp=true \
-    media.stagefright.enable-scan=true \
-    mmp.enable.3g2=true \
     af.resampler.quality=255 \
-    ro.opengles.version=131072 \
-    mpq.audio.decode=true
+    ro.opengles.version=131072
 
 #misc
 PRODUCT_PROPERTY_OVERRIDES += \
