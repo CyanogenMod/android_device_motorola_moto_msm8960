463c16a1e49210021ba473090da375b8f92be4c0
dhacker29 <davidhackerdvm@gmail.com>
2013-07-29 01:04:23 -0500

    qcom/media: Import cm-10.2 commit 17675690740a6c176758c761406b4c0512cef6db
    Change-Id: I9aa570c26d4927c769df421998b5dcee9aed7087

---
+++ modules/media/dashplayer/Android.mk
@@ -0,0 +1,51 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+# ---------------------------------------------------------------------------------
+#            Common definitons
+# ---------------------------------------------------------------------------------
+LOCAL_SRC_FILES:=                       \
+        DashPlayer.cpp                  \
+        DashPlayerDriver.cpp            \
+        DashPlayerRenderer.cpp          \
+        DashPlayerStats.cpp             \
+        DashPlayerDecoder.cpp           \
+        DashPacketSource.cpp            \
+        DashFactory.cpp                 \
+        DashCodec.cpp
+
+LOCAL_SHARED_LIBRARIES :=       \
+    libbinder                   \
+    libcamera_client            \
+    libcutils                   \
+    libdl                       \
+    libgui                      \
+    libmedia                    \
+    libstagefright              \
+    libstagefright_foundation   \
+    libstagefright_omx          \
+    libutils                    \
+    libui                       \
+
+LOCAL_STATIC_LIBRARIES :=       \
+    libstagefright_nuplayer     \
+    libstagefright_rtsp         \
+
+LOCAL_C_INCLUDES := \
+    $(TOP)/frameworks/av/media/libstagefright/timedtext           \
+	$(TOP)/frameworks/native/include/media/hardware               \
+	$(TOP)/frameworks/native/include/media/openmax                \
+	$(TOP)/frameworks/av/media/libstagefright/httplive            \
+	$(TOP)/frameworks/av/media/libmediaplayerservice/nuplayer     \
+	$(TOP)/frameworks/av/media/libmediaplayerservice              \
+	$(TOP)/frameworks/av/media/libstagefright/include             \
+	$(TOP)/frameworks/av/media/libstagefright/mpeg2ts             \
+	$(TOP)/frameworks/av/media/libstagefright/rtsp                \
+	$(TOP)/hardware/qcom/media/mm-core/inc                        \
+
+LOCAL_MODULE:= libdashplayer
+
+LOCAL_MODULE_TAGS := eng
+
+include $(BUILD_SHARED_LIBRARY)
+
---
+++ modules/media/libc2dcolorconvert/Android.mk
@@ -0,0 +1,26 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+ifeq ($(TARGET_QCOM_DISPLAY_VARIANT),caf)
+DISPLAY := display-caf
+else
+DISPLAY := display
+endif
+
+LOCAL_SRC_FILES := \
+        C2DColorConverter.cpp
+
+LOCAL_C_INCLUDES := \
+    $(TOP)/frameworks/av/include/media/stagefright \
+    $(TOP)/frameworks/native/include/media/openmax \
+    $(TOP)/hardware/qcom/$(DISPLAY)/libcopybit \
+    $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
+
+LOCAL_SHARED_LIBRARIES := liblog libdl
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_MODULE := libc2dcolorconvert
+
+include $(BUILD_SHARED_LIBRARY)
