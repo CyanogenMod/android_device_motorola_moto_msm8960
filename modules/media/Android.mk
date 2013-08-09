48f4118e96cd36dd38f2c67af58b494243f2129b
dhacker29 <davidhackerdvm@gmail.com>
2013-08-09 00:38:48 -0500

    Revert local copy of qcom/media. All commits are merged now
    Change-Id: Ia6c02d7feafd1cb4ead2c0cf2145f73bc00dc1f4

--- modules/media/Android.mk
+++
@@ -1,9 +0,0 @@
-ifneq ($(filter msm8960 msm8660 msm7x30,$(TARGET_BOARD_PLATFORM)),)
-ifneq ($(TARGET_QCOM_DISPLAY_VARIANT),legacy)
-ifeq ($(TARGET_QCOM_MEDIA_VARIANT),moto)
-
-include $(call all-subdir-makefiles)
-
-endif
-endif
-endif
