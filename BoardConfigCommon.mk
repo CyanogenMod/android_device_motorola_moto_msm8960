bd3751bd87b13e30692e74191cc6a26152f08317
dhacker29 <davidhackerdvm@gmail.com>
2013-12-27 19:06:02 -0600

    Move TARGET_RECOVERY_PIXEL_FORMAT to family
    Change-Id: Ia3ba6f17fae36a1cb537a7303b1c11fee69aee3e

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -68,6 +68,7 @@ BOARD_EGL_CFG := $(LOCAL_PATH)/config/egl.cfg
 TARGET_OTA_ASSERT_DEVICE := xt925,xt926,xt907,vanquish_u,vanquish,scorpion_mini,mb886,qinara,asanti,asanti_c,xt897,xt897c
 
 # Recovery
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBA_8888"
 TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/etc/fstab.qcom
 
 # TWRP
