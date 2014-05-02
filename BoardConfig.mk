73d8548562aaabf6e3dd1dbbfed7097b598a58a8
dhacker29 <dhackerdvm@gmail.com>
2014-05-02 11:10:50 -0400

    Revert "selinux: Enable enforcing"
    This is breaking all sensors I need to debug more
    
    This reverts commit fd390d50dd5426ba0071763cd2428007d82d529d.
    
    Change-Id: Ibb56496e288a859387f6bae2869b2200111461b4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -44,7 +44,7 @@ TARGET_LIBINIT_DEFINES_FILE := device/motorola/moto_msm8960/init/init_moto_msm89
 TARGET_KERNEL_SOURCE := kernel/motorola/msm8960-common
 TARGET_KERNEL_CONFIG := msm8960_mmi_defconfig
 TARGET_KERNEL_SELINUX_CONFIG := msm8960_mmi_selinux_defconfig
-BOARD_KERNEL_CMDLINE := console=/dev/null androidboot.hardware=qcom user_debug=31 loglevel=1 zcache
+BOARD_KERNEL_CMDLINE := console=/dev/null androidboot.hardware=qcom user_debug=31 loglevel=1 zcache androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80200000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01600000
