b2aca983138f53a0083d47746214c8a9ea522ed3
dhacker29 <dhackerdvm@gmail.com>
2014-05-02 10:55:30 -0400

    root: msm8960 needs mtp,usbnet,adb
    Change-Id: Id42b53ca6541344bf496cb6e9b878466055c0e95

--- device_moto_msm8960.mk
+++ device_moto_msm8960.mk
@@ -84,6 +84,10 @@ PRODUCT_PACKAGES += \
     init.qcom.post_boot.sh \
     init.qcom.sh
 
+# Ramdisk overrides
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/init.qcom.usb.rc:root/init.qcom.usb.rc
+
 # TWRP
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/twrp.fstab:recovery/root/etc/twrp.fstab
