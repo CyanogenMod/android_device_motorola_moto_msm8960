9756b54b326ebdc404d36e65a01468dd6ac5128d
dhacker29 <dhackerdvm@gmail.com>
2014-05-09 05:07:59 -0400

    recovery: Set TARGET_RECOVERY_NO_MSM_BSP for compiling CM recovery
    Change-Id: I6dc6739c693c2a1137ef5e460da65fd13b7e42c4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -80,6 +80,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := device/motorola/moto_msm8960
 TARGET_OTA_ASSERT_DEVICE := moto_msm8960,xt925,xt926,xt907,vanquish_u,vanquish,scorpion_mini,mb886,qinara,asanti,asanti_c,xt897,xt897c
 
 # Recovery
+TARGET_RECOVERY_NO_MSM_BSP := true
 BOARD_CUSTOM_GRAPHICS := ../../../$(LOCAL_PATH)/graphics.c
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBA_8888"
 TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/etc/fstab.qcom
