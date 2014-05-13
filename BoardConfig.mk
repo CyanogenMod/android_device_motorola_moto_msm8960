339a005e190024eb55380cb2dcd9ddf8bb562fcd
dhacker29 <dhackerdvm@gmail.com>
2014-05-13 02:45:23 -0400

    Add asserts for xt901 and xt905
    Change-Id: I6bc0045ba762facd20977d309a0db51d7465d7c7

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -77,7 +77,7 @@ BOARD_EGL_CFG := $(LOCAL_PATH)/config/egl.cfg
 TARGET_RELEASETOOLS_EXTENSIONS := device/motorola/moto_msm8960
 
 # Assert
-TARGET_OTA_ASSERT_DEVICE := moto_msm8960,xt925,xt926,xt907,vanquish_u,vanquish,scorpion_mini,mb886,qinara,asanti,asanti_c,xt897,xt897c
+TARGET_OTA_ASSERT_DEVICE := moto_msm8960,xt901,solstice,xt905,smq_u,xt907,scorpion_mini,xt925,vanquish_u,xt926,vanquish,mb886,qinara,xt897,asanti,xt897c,asanti_c
 
 # Recovery
 BOARD_CUSTOM_GRAPHICS := ../../../$(LOCAL_PATH)/graphics.c
