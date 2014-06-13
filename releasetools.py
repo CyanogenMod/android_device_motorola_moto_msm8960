d93428703e8352cf21a4370db4f4fbe1762aaa0d
dhacker29 <dhackerdvm@gmail.com>
2014-06-13 00:08:31 -0400

    Assert that this device has a 4.4 bootloader before installing
    As moto didn't change the ro.bootloader.version prop, we can use
    ro.boot.secure_hardware=1, since 4.1.2 devices didn't not pass
    this on the commandline.
    
    Change-Id: Ie6fad9fcde9fbb8852a441ad17c0ed9af3d68532

--- releasetools.py
+++ releasetools.py
@@ -15,6 +15,9 @@
 
 """Custom OTA commands for Motorola msm8960 devices"""
 
+def FullOTA_InstallBegin(info):
+	info.script.AppendExtra('assert(getprop("ro.boot.secure_hardware") == "1" || abort("This package is for devices with 4.4 bootloader"););')
+
 def FullOTA_InstallEnd(info):
 	info.script.AppendExtra('ifelse(is_substring("smq", getprop("ro.boot.device")) || is_substring("XT90", getprop("ro.boot.modelno")) || is_substring("XT90", getprop("ro.product.device")), run_program("/sbin/sh", "-c", "busybox mv /system/etc/media_profiles_xt90x.xml /system/etc/media_profiles.xml"));')
 	info.script.AppendExtra('ifelse(is_substring("MB886", getprop("ro.boot.modelno")) || is_substring("att", getprop("ro.boot.carrier")), run_program("/sbin/sh", "-c", "busybox rm /system/etc/nfc* /system/etc/permissions/*nfc* /system/etc/permissions/*nxp* /system/lib/libnfc* /system/lib/hw/nfc* /system/framework/com.android.nfc_extras.jar /system/app/Nfc.apk /system/priv-app/Tag.apk"));')
