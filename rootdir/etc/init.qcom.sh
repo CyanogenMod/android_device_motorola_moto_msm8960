dcf4c10941a96db97ebaa0271fb1d8d1d941d979
Ravinder Konka <rkonka@codeaurora.org>
2014-11-13 20:04:29 +0530

    qcom: Enabling default route addition during RA in IPv6
    Removing the code which sets accept_ra_defrtr to 0 for all interfaces
    
    Change-Id: I280a317f21190420a10bb2abfc479749d75ec15b

--- rootdir/etc/init.qcom.sh
+++ rootdir/etc/init.qcom.sh
@@ -65,14 +65,6 @@ start_battery_monitor()
 baseband=`getprop ro.baseband`
 izat_premium_enablement=`getprop ro.qc.sdk.izat.premium_enabled`
 
-#
-# Suppress default route installation during RA for IPV6; user space will take
-# care of this
-# exception default ifc
-for file in /proc/sys/net/ipv6/conf/*
-do
-  echo 0 > $file/accept_ra_defrtr
-done
 echo 1 > /proc/sys/net/ipv6/conf/default/accept_ra_defrtr
 
 #
