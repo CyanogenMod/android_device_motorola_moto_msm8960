792d369ec61106825870698e8a19e5e2fe4d65a7
dhacker29 <dhackerdvm@gmail.com>
2014-12-07 02:31:23 -0500

    releasetools: Mount system before doing FullOTA_InstallEnd commands
    Change-Id: Icb9e6ff3e81ff3c1435149cc1b7899673d1ee476

--- releasetools.py
+++ releasetools.py
@@ -19,6 +19,7 @@ def FullOTA_InstallBegin(info):
 	info.script.AppendExtra('assert(getprop("ro.boot.secure_hardware") == "1" || abort("This package is for devices with 4.4 bootloader"););')
 
 def FullOTA_InstallEnd(info):
+	info.script.Mount("/system")
 	info.script.AppendExtra('ifelse(is_substring("smq", getprop("ro.boot.device")) || is_substring("XT90", getprop("ro.boot.modelno")) || is_substring("XT90", getprop("ro.product.device")), run_program("/sbin/sh", "-c", "busybox mv /system/etc/media_profiles_xt90x.xml /system/etc/media_profiles.xml"));')
 	info.script.AppendExtra('ifelse(is_substring("MB886", getprop("ro.boot.modelno")) || is_substring("att", getprop("ro.boot.carrier")), run_program("/sbin/sh", "-c", "busybox rm /system/etc/nfc* /system/etc/permissions/*nfc* /system/etc/permissions/*nxp* /system/lib/libnfc* /system/lib/hw/nfc* /system/framework/com.android.nfc_extras.jar /system/app/Nfc.apk /system/priv-app/Tag.apk"));')
 	info.script.AppendExtra('delete("/system/etc/media_profiles_xt90x.xml");')
@@ -27,3 +28,4 @@ def FullOTA_InstallEnd(info):
 	info.script.AppendExtra('ifelse(is_substring("XT901", getprop("ro.boot.modelno")), run_program("/sbin/sh", "-c", "busybox cp -R /system/xt901/* /system/"));')
 	info.script.AppendExtra('ifelse(is_substring("XT92", getprop("ro.boot.modelno")) || is_substring("vanquish", getprop("ro.boot.device")) || is_substring("vanquish", getprop("ro.product.device")) || is_substring("xt92", getprop("ro.product.device")), run_program("/sbin/sh", "-c", "busybox mv /system/etc/snd_soc_msm/snd_soc_msm_2x_xt92x /system/etc/snd_soc_msm/snd_soc_msm_2x"));')
 	info.script.AppendExtra('delete("/system/etc/snd_soc_msm/snd_soc_msm_2x_xt92x");')
+	info.script.Unmount("/system")
